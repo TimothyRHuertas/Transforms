6c347d76a6b861f6925b9031148d810b50a620e6
TImothy Huertas <TimothyRHuertas@gmail.com>
2024-01-28 11:38:46 -0800

    position

--- Transforms/PositionView.swift
+++ Transforms/PositionView.swift
@@ -9,31 +9,111 @@ import SwiftUI
 import RealityKit
 import RealityKitContent
 
+enum Arrangements: String, CaseIterable {
+    case xyCircle = "Circle XY"
+    case yzCircle = "Circle YZ"
+    case xzCircle = "Circle XZ"
+}
+
+@Observable
+class ViewModel {
+    var arrangement:Arrangements = .xyCircle
+}
+
+let viewModel = ViewModel()
+
+struct SettingsMenuView: View {
+    var body: some View {
+        VStack {
+            ForEach(Array(Arrangements.allCases), id: \.self) {
+                value in
+                
+                Button(value.rawValue) {
+                    Task {
+                        viewModel.arrangement = value
+                        print("oops", value)
+                    }
+                    
+                }
+            }
+        }
+    }
+    
+}
+
 struct PositionView: View {
+    let settingsMenuTag = "settingsMenu"
+    @State private var gizmos:[Entity] = .init()
+    let max:Float = 2.0 * .pi
+    let steps = 10
+    
     private func buildSphere(_ radius:Float, _ color:UIColor) -> Entity {
         let material = SimpleMaterial.init(color: color, isMetallic: true)
         let mesh = MeshResource.generateSphere(radius: radius)
         let entity = ModelEntity(mesh: mesh, materials: [material])
-        entity.components.set(DragRotateComponent())
-        entity.components.set(DragTransformComponent())
+//        entity.components.set(DragRotateComponent())
+//        entity.components.set(DragTransformComponent())
 
         return entity
     }
     
-    var body: some View {
-        RealityView { content in
-            let max:Float = 2.0 * .pi
-            let steps = 10
+    private func layoutGizmos(_ animate:Bool = false) {
+        gizmos.enumerated().forEach {
+            index, gizmo in
             
-            for step in 1...steps {
-                let radius:Float = 1
-                let radians = Float(step) * max/Float(steps)
-                let x = radius * cos(radians)
-                let y = radius * sin(radians)
+            let radius:Float = 1
+            let radians = Float(index) * max / Float(gizmos.count)
+            let x = radius * cos(radians)
+            let y = radius * sin(radians)
+                        
+            let position:simd_float3 = switch viewModel.arrangement {
+                case .xyCircle:
+                    [x, y, 0] + [0, 1.8, -2]
+                case .yzCircle:
+                    [x, 0, y] + [0, 1.8, -2]
+                case .xzCircle:
+                    [0, x, y] + [0, 1.8, -2]
+            }
+            
+            if animate {
+                var destination = gizmo.transform
+                destination.translation = position
+                gizmo.move(to: destination, relativeTo: gizmo.parent, duration: 1, timingFunction: .easeInOut)
+            }
+            else {
+                gizmo.position = position
+            }
+        }
+    }
+    
+    var body: some View {
+        RealityView { content, attachments in
+            let headAnchor = AnchorEntity(.head)
+            headAnchor.position = [0.75, 0.3, -1]
+            
+            if let settingsMenu = attachments.entity(for: settingsMenuTag) {
+                headAnchor.addChild(settingsMenu)
+                content.add(headAnchor)
+            }
+            
+            for _ in 1...steps {
                 let gizmo = buildSphere(0.1, .gray)
-                gizmo.position = [x, y, 0] + [0, 1.8, -2]
+                gizmos.append(gizmo)
                 content.add(gizmo)
             }
+            
+            layoutGizmos()
+        }
+        attachments: {
+            Attachment(id: settingsMenuTag) {
+                SettingsMenuView()
+                .padding(40)
+                .glassBackgroundEffect()
+            }
+            
+        }
+        .onChange(of: viewModel.arrangement) {
+            layoutGizmos(true)
         }
         .dragRotation()
         .dragParent()
