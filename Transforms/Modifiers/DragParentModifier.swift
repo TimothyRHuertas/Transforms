7d36e7a7788627ab3dab580c35fb985b0fc3cab0
TImothy Huertas <TimothyRHuertas@gmail.com>
2024-01-27 17:00:58 -0800

    wip

--- Transforms/Modifiers/DragParentModifier.swift
+++ Transforms/Modifiers/DragParentModifier.swift
@@ -18,24 +18,16 @@ extension View {
 
 /// A modifier converts drag gestures into entity rotation.
 private struct DragParentModifier: ViewModifier {
-    @State private var previousDragLocation:simd_float3?
-
     func body(content: Content) -> some View {
         let dragGesture = DragGesture(minimumDistance: 0.0)
             .targetedToEntity(where: .has(DragParentComponent.self))
         .onChanged { value in
             let location3D = value.convert(value.location3D, from: .local, to: .scene)
-            
-            if let previousDragLocation = previousDragLocation {
-                value.entity.components[DragParentComponent.self]?.delta = location3D - previousDragLocation
-            }
-            
-            previousDragLocation = location3D
+            value.entity.components[DragParentComponent.self]?.delta = location3D - value.convert(value.startLocation3D, from: .local, to: .scene)
         }
         .onEnded {
             value in
             value.entity.components[DragParentComponent.self]?.delta = nil
-            previousDragLocation = nil
         }
         
         content.gesture(dragGesture)
