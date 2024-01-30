32e551b836f5f506a7309d399a7ee9c1e0a4bc77
TImothy Huertas <TimothyRHuertas@gmail.com>
2024-01-30 14:36:33 -0800

    wip

--- Transforms/Systems/RotateToHitSystem.swift
+++ Transforms/Systems/RotateToHitSystem.swift
@@ -7,10 +7,11 @@
 
 import RealityKit
 import UIKit
+import ARKit
 
 public struct RotateToHitSystem: System {
     public static var dependencies: [SystemDependency] {
-        [.after(DragRotateSystem.self), .after(DragParentSystem.self )]
+        [.after(DragRotateSystem.self), .after(DragParentSystem.self), .before(LineSystem.self)]
     }
     
     static let query = EntityQuery(where: .has(RotateToHitComponent.self))
@@ -18,14 +19,40 @@ public struct RotateToHitSystem: System {
     public init(scene: RealityKit.Scene) {
     }
     
+    private func drawLine(_ entity:Entity, _ name:String, _ from:simd_float3, _ to:simd_float3) {
+        let lineComponent = LineComponent(from: from, to: to)
+        if let line = entity.findEntity(named: name) {
+            line.components.set(lineComponent)
+        }
+        else {
+            let line = ModelEntity()
+            line.components.set(lineComponent)
+            line.name = name
+            entity.addChild(line)
+        }
+    }
+    
     public func update(context: SceneUpdateContext) {
         let scene = context.scene
         let entities = scene.performQuery(Self.query)
         
         for entity in entities {
-            if let component =  entity.components[RotateToHitComponent.self], let parent = entity.parent {
-               print("TODO")
+            guard let parent = entity.parent else {return}
+            
+            let position = entity.position
+            let entityDown = position - [0, 1, 0]
+            drawLine(parent, "downLine", position, entityDown)
+            let result = scene.raycast(from: position, to: entityDown)
+            let floorHit = result.first(where: {$0.entity.name == "floor"})
+            
+            if let floorHit = floorHit {
+                print("tim", floorHit.entity.name)
+                let normal = floorHit.normal//entity.convert(normal: floorHit.normal, to: nil)
+                drawLine(parent, "normal", position, normal)
+
+                entity.transform.rotation *= simd_quatf(from: [0, 1, 0], to: normal)
             }
+            
         }
     }
 }
--- Transforms/Views/RotateToMatchFloorView.swift
+++ Transforms/Views/RotateToMatchFloorView.swift
@@ -29,7 +29,7 @@ struct RotateToMatchFloorView: View {
             let material = SimpleMaterial.init(color: .gray, isMetallic: true)
             let mesh = MeshResource.generateBox(size: 0.2)
             let entity = ModelEntity(mesh: mesh, materials: [material])
-            entity.position = [1, 2.0, -2]
+            entity.position = [1, 1.5, -2]
             entity.components.set(DragTransformComponent())
             entity.transform.rotation = simd_quatf(.init(angle: .degrees(-90), axis: .y))
             var dragRotateComponent = DragRotateComponent()
@@ -41,7 +41,8 @@ struct RotateToMatchFloorView: View {
             let hillMaterial = SimpleMaterial.init(color: .gray, isMetallic: true)
             let hillMesh = MeshResource.generateSphere(radius: 0.8)
             let hillEntity = ModelEntity(mesh: hillMesh, materials: [hillMaterial])
-            hillEntity.position = [-1, 2.0, -2]
+            hillEntity.position = [-1, 0, -2]
+            hillEntity.name = "floor"
             makeCollidable(hillEntity)
             content.add(hillEntity)
         }
