877b8cd52388f9cb2e45b79b88dec54e445c49c3
TImothy Huertas <TimothyRHuertas@gmail.com>
2024-01-30 13:02:57 -0800

    wip

--- Transforms/Views/RotateToMatchFloorView.swift
+++ Transforms/Views/RotateToMatchFloorView.swift
@@ -16,13 +16,19 @@ struct RotateToMatchFloorView: View {
             let material = SimpleMaterial.init(color: .gray, isMetallic: true)
             let mesh = MeshResource.generateBox(size: 0.2)
             let entity = ModelEntity(mesh: mesh, materials: [material])
-            entity.position = [0, 2.0, -2]
+            entity.position = [1, 2.0, -2]
             entity.components.set(DragTransformComponent())
-            
+            entity.transform.rotation = simd_quatf(.init(angle: .degrees(-90), axis: .y))
             var dragRotateComponent = DragRotateComponent()
-            dragRotateComponent.rotateY = false
+            dragRotateComponent.rotateX = false
             entity.components.set(dragRotateComponent)
             content.add(entity)
+            
+            let hillMaterial = SimpleMaterial.init(color: .gray, isMetallic: true)
+            let hillMesh = MeshResource.generateSphere(radius: 0.8)
+            let hillEntity = ModelEntity(mesh: hillMesh, materials: [hillMaterial])
+            hillEntity.position = [-1, 2.0, -2]
+            content.add(hillEntity)
         }
         .dragParent()
         .dragRotation()
